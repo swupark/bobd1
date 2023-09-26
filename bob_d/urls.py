71368f5fdfe25dc4acc6112a90cad11a8659004e
leemk <pallang719@naver.com>
2023-09-26 17:10:19 +0900

    git ignore

--- bob_d/urls.py
+++ bob_d/urls.py
@@ -16,15 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf.urls import include
 from signupapp.views import Signupview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
     path('signup/',Signupview.as_view()),
     path('account/',include('accountapp.urls'))
-
-=======
->>>>>>> parent of 45ecb78 (signupp create)
 ]
