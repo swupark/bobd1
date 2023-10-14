266300e66ecc3366199856a85b14b0df0e4f11e8
SJ-Hyun <gusth1111@swu.ac.kr>
2023-10-14 14:58:46 +0900

    mypageapp 생성

--- bob_d/settings.py
+++ bob_d/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accountapp',
     'signupapp',
+    'mypageapp',
     'bootstrap4'
 ]
 
---
+++ mypageapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MypageappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mypageapp'
