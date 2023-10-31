3a772e7906ff53f006707b26ac88be908595199f
leemk <pallang719@naver.com>
2023-10-31 15:16:57 +0900

    logout 성공, 모델 수정해야함

--- signupapp/forms.py
+++ signupapp/forms.py
@@ -1,12 +1,12 @@
 from django.forms import ModelForm
 from django import forms
 
-from signupapp.models import Account
+#from signupapp.models import Account
 
 
-class SignuppForm(ModelForm):
-    class Meta:
-        model=Account
-        fields=['nickname','username','password','age','gender','issue_ls','issue_hp',
-               'issue_lk','issue_vg']
+#class SignuppForm(ModelForm):
+#    class Meta:
+#        model=Account
+#        fields=['nickname','username','password','age','gender','issue_ls','issue_hp',
+#               'issue_lk','issue_vg']
 
